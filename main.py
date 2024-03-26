097b77b6b149188741a3fd41725db2cb2b6a80b2
anzieri <amaranyanzi1@gmail.com>
2024-03-26 16:36:04 +0300

    Single server/ Multi server

--- main.py
+++ main.py
@@ -10,38 +10,38 @@ def hello():
     return "Hello, World!"
 
 # # Store active connections
-# active_connections = []
+active_connections = []
 
-# @app.route('/send', methods=['POST'])
-# def send_message():
-#     data = request.form.get('message')
-#     if data:
-#         for conn in active_connections:
-#             conn.send(data.encode())
-#         return "Message sent to all clients."
-#     else:
-#         return "No message provided."
+@app.route('/send', methods=['POST'])
+def send_message():
+    data = request.form.get('message')
+    if data:
+        for conn in active_connections:
+            conn.send(data.encode())
+        return "Message sent to all clients."
+    else:
+        return "No message provided."
 
-# def handle_client(conn):
-#     while True:
-#         data = conn.recv(1024).decode()
-#         if not data:
-#             break
-#         print(f"Received from client: {data}")
-#         # You can process the data here or send it to other clients
-#         # For now, we'll just echo it back to the sender
-#         conn.send(data.encode())
-#     conn.close()
+def handle_client(conn):
+    while True:
+        data = conn.recv(1024).decode()
+        if not data:
+            break
+        print(f"Received from client: {data}")
+        # You can process the data here or send it to other clients
+        # For now, we'll just echo it back to the sender
+        conn.send(data.encode())
+    conn.close()
 
-# @app.route('/connect', methods=['POST'])
-# def connect_client():
-#     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     client_socket.connect((get_server_ip(), port))
-#     active_connections.append(client_socket)
-#     return "Connected to server."
+@app.route('/connect', methods=['POST'])
+def connect_client():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((get_server_ip(), port))
+    active_connections.append(client_socket)
+    return "Connected to server."
 
-# def get_server_ip():
-#     return socket.gethostbyname(socket.gethostname())
+def get_server_ip():
+    return socket.gethostbyname(socket.gethostname())
 
 if __name__ == '__main__':
     # rules = hello()
